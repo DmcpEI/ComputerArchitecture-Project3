cdac447d8f214fb45528b08f27303bcdf4ffa40f
Diogo Paixão <diogomcpaixao@gmail.com>
2024-05-17 19:08:58 +0100

    C quase feito, falta parte de não escolher nada

--- ProjetoC/ProjetoC.c
+++ ProjetoC/ProjetoC.c
@@ -50,44 +50,197 @@ sbit D2DF = P2^7;
 //conta = 1 -> 250 microsegundos
 //conta = 400 -> 0.1 segundo
 //conta = 20000 -> 5 segundos
-int conta = 0;
-int segundos = 0;
+unsigned int conta = 0;
 
+int segundosIniciais = TempoInicial;
+int segundo = 12000;
+
+// Está a '0' se está a ser mostrado o tempo inicial nos displays
+// Está a '1' se informação com o tempo/resposta do participante esteja a ser mostrada nos displays
+bit respondeu = 0;
+
+bit pressionou = 0;
+
+int resposta = 0;
+
+bit opcao = 0;
+
+code unsigned segments[22][8] = {
+	{1, 1, 1, 1, 1, 1, 0, 0}, // -.
+	{0, 0, 0, 0, 0, 0, 1, 0}, // 0.
+	{1, 0, 0, 1, 1, 1, 1, 0}, // 1.
+	{0, 0, 1, 0, 0, 1, 0, 0}, // 2.
+	{0, 0, 0, 0, 1, 1, 0, 0}, // 3.
+	{1, 0, 0, 1, 1, 0, 0, 0}, // 4.
+	{0, 1, 0, 0, 1, 0, 0, 0}, // 5.
+	{1, 1, 1, 1, 1, 1, 0, 1}, // -
+	{0, 0, 0, 0, 0, 0, 1, 1}, // 0
+	{1, 0, 0, 1, 1, 1, 1, 1}, // 1
+	{0, 0, 1, 0, 0, 1, 0, 1}, // 2
+	{0, 0, 0, 0, 1, 1, 0, 1}, // 3
+	{1, 0, 0, 1, 1, 0, 0, 1}, // 4
+	{0, 1, 0, 0, 1, 0, 0, 1}, // 5
+	{0, 1, 0, 0, 0, 0, 0, 1}, // 6
+	{0, 0, 0, 1, 1, 1, 1, 1}, // 7
+	{0, 0, 0, 0, 0, 0, 0, 1}, // 8
+	{0, 0, 0, 0, 1, 0, 0, 1}, // 9
+	{0, 0, 0, 1, 0, 0, 0, 1}, // A
+	{1, 1, 0, 0, 0, 0, 0, 1}, // B
+	{0, 1, 1, 0, 0, 0, 1, 1}, // C
+	{1, 0, 0, 0, 0, 1, 0, 1}, // D
+};
+
+void Init(void);
+void displaySegundos(int num);
+void verificaPressionado(void);
+void semResposta(void);
+void mostraInformacao(void);
+
+void main (void)
+{
+	Init();
+	
+	while(1){
+		
+		displaySegundos(segundosIniciais);
+		verificaPressionado();
+		
+		if (segundosIniciais == 0){
+			semResposta();
+		}
+		
+		if (pressionou){
+			mostraInformacao();
+		}
+	}
+}
 
 void Init (void)
 {
-	EA = 1;
-	ET0 = 1;
-	EX0 = 1;
+	// Configuração do registo
+	EA = 1; // Ativa as interrupções globais
+	ET0 = 1; // Ativa a interrupção timer 0
+	EX0 = 1; // Ativa a interrupção externa 0
 	
+	// Timer no modo 2, de 8 bits
 	TMOD &= 0xF0;
 	TMOD |= 0x02;
 	
 	TH0 = TempoH;
 	TL0 = TempoL;
 	
-	TR0 = 1;
+	IT0 = 1; // Interrupcao externa 0 activa a falling edge
+	TR0 = 0;
+}
+
+void External0 (void) interrupt 0 
+{
+	if(respondeu){
+		TR0 = 0; // Timer0 para de contar o tempo
+		segundosIniciais = TempoInicial;
+		conta = 0;
+		respondeu = 0;
+	} 
+	else {
+		TR0 = 1; // Timer0 começa a contar tempo
+		respondeu = 1;
+	}
 }
 
 void Timer0_ISR (void) interrupt 1 
 {
 	conta++;
-	
-	if (conta == 400){
-		segundos++;
-		conta=0;
-	}
-	
-	if (segundos == 50){
-		TR0=0;
+
+	if (!pressionou){
+		if (conta == 400){
+			segundosIniciais--;
+			conta=0;
+		}
 	}
 }
 
-void main (void)
+void display (int num1, int num2){
+	D1A = segments[num1][0];
+	D1B = segments[num1][1];
+	D1C = segments[num1][2];
+	D1D = segments[num1][3];
+	D1E = segments[num1][4];
+	D1F = segments[num1][5];
+	D1G = segments[num1][6];
+	D1DF = segments[num1][7];
+
+	D2A = segments[num2][0];
+	D2B = segments[num2][1];
+	D2C = segments[num2][2];
+	D2D = segments[num2][3];
+	D2E = segments[num2][4];
+	D2F = segments[num2][5];
+	D2G = segments[num2][6];
+	D2DF = segments[num2][7];
+}
+
+void displaySegundos (int num)
 {
-	Init();
-	
-	while(TR0){
-		P0 = segundos;
+    int dezenas = num / 10;
+    int unidades = num % 10;
+
+		display(dezenas+1, unidades+8);
+}
+
+void verificaPressionado(void)
+{
+	if (!BA){
+		pressionou = 1;
+		resposta = 1;
+	} else if (!BB){
+		pressionou = 1;
+		resposta = 2;
+	} else if (!BC){
+		pressionou = 1;
+		resposta = 3;
+	} else if (!BD){
+		pressionou = 1;
+		resposta = 4;
+	} else {
+		pressionou = 0;
 	}
+}
+
+void semResposta (void)
+{
+	pressionou = 1;
+	conta = 0;
+	display(1, 8);
+	TR0 = 1;
+	
+	while (B1){
+		if (conta == segundo){
+			display(0, 7);
+		}
+	}
+	
+	TR0 = 0;
+	conta = 0;
+}
+
+void mostraInformacao(void)
+{
+	opcao = 0;
+	TR0 = 1;
+	
+	while (B1){
+		if (conta == segundo){
+			if (opcao == 0){
+				displaySegundos(segundosIniciais);
+				opcao = 1;
+			} else {
+				display(0, resposta+17);
+				opcao = 0;
+			}
+			conta = 0;
+		}
+	}
+	
+	TR0 = 0;
+	conta = 0;
 }
